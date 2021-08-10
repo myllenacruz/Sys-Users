4508d66a907aa898ef3425ba238c4e2f7ee2c315
Myllena Cruz <myllena12345@hotmail.com>
2021-08-10 16:26:23 -0300

    CRUD + Middleware

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -3,5 +3,9 @@ const router = express.Router()
 const UserController = require('../controllers/UserController')
 
 router.get('/users', UserController.index)
+router.post('/users', UserController.create)
+router.put('/users/:id', UserController.update)
+router.delete('/users/:id', UserController.delete)
+
 
 module.exports = router
--- src/server.js
+++ src/server.js
@@ -2,7 +2,19 @@ const express = require('express')
 const app = express()
 const router = require('./routes/userRoute')
 
+app.use(express.json())
 app.use(router)
+
+app.use((req, res, next) => {
+  const error = new Error('Not found!')
+  error.status = 404
+  next(error)
+})
+
+app.use((req, res, next) => {
+  res.status(error.status || 500)
+  res.json({ error: error.message })
+})
 
 app.listen(3333, () => {
   console.log('Ok...')
